6786c16bfec1698a913b5f2972b8b563bd371857
Barsaa75 <110914653+Barsaa75@users.noreply.github.com>
2023-05-14 21:23:48 +0800

    pzda

--- components/header/HeaderSection.tsx
+++ components/header/HeaderSection.tsx
@@ -10,7 +10,7 @@ const HeaderSection = () => {
       <div
         className={`fixed left-0 top-0 z-10 h-[80px] w-full px-8 py-6 backdrop-blur`}
       >
-        <div className="container flex justify-between ">Hi</div>
+        <div className="container flex justify-between ">Hi llra</div>
       </div>
     </>
   );
